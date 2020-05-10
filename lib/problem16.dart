e2e4430c361ebdfb96a9c7e4c4a235d39c611d19
Angelo Giacco <giacco.angelo.s@gmail.com>
2020-05-10 16:17:02 +0100

    add explanation and remove unnecessary import

--- lib/problem16.dart
+++ lib/problem16.dart
@@ -1,5 +1,8 @@
-import 'dart:math';
+/*
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
+What is the sum of the digits of the number 21000?
+*/
 void main() {
   BigInt power = BigInt.parse("2").pow(1000);
   List<String> digits = power.toString().split("");
