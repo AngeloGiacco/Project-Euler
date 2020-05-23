b41e80efb8f2e3b489c5bc224599d257abf99e61
Angelo Giacco <giacco.angelo.s@gmail.com>
2020-05-23 22:18:38 +0100

    Update problem32.dart

--- lib/problem21-40/problem32.dart
+++ lib/problem21-40/problem32.dart
@@ -7,6 +7,7 @@ Find the sum of all products whose multiplicand/multiplier/product identity can 
 
 HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
 */
+library euler.problem32;
 import "package:trotter/trotter.dart";
 
 final items = characters('123456789'),
